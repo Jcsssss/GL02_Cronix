f22b86b5bca5f6604f2b86c43f7c28ba28edac76
Jcsssss <charletj@utt.fr>
2025-12-07 21:12:39 +0100

    Update config.js
    ajouts path

--- src/config/config.js
+++ src/config/config.js
@@ -9,12 +9,16 @@ export const PROJECT_ROOT = path.resolve(__dirname, "../..");
 export const REVIEW_DIR  = path.join(PROJECT_ROOT, "review");
 export const RESULTS_DIR = path.join(PROJECT_ROOT, "results");
 export const DATA_DIR    = path.join(PROJECT_ROOT, "data");
-export const VCARDS_DIR  = path.join(PROJECT_ROOT, "results/vcards");
+export const PROFILS_DIR = path.join(PROJECT_ROOT, "profils");
+export const EXPORT_PROFILAGE_DIR = path.join(PROJECT_ROOT, "exports_profilage_png_csv");
+export const VCARDS_DIR  = path.join(PROJECT_ROOT, "vCards");
 
 export default {
   PROJECT_ROOT,
   REVIEW_DIR,
   RESULTS_DIR,
   DATA_DIR,
+  PROFILS_DIR,
+  EXPORT_PROFILAGE_DIR,
   VCARDS_DIR
 };
